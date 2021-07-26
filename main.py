6149dc2056d75527b9ed8c499b5a14f85286d800
Yuutokata <77214089+Yuutokata@users.noreply.github.com>
2021-07-26 23:24:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,26 +18,6 @@ async def on_ready():
     print("Bot on")
     DiscordComponents(client)
 
-@client.command()
-@commands.is_owner()
-async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
-    await ctx.send(f"`{extension}` wurde **geladen**")
-
-@client.command()
-@commands.is_owner()
-async def unload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-    await ctx.send(f"`{extension}` wurde **entladen**")
-
-@client.command()
-@commands.is_owner()
-async def reload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-    await asyncio.sleep(3)
-    client.load_extension(f'cogs.{extension}')
-    await ctx.send(f"`{extension}` wurde **neugeladen**")
-
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
