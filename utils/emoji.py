c3ce0cd8403022b22521827e36f4d3e641cba9fc
Yuutokata <77214089+Yuutokata@users.noreply.github.com>
2021-07-30 00:38:25 +0200

    Create emoji.py

---
+++ utils/emoji.py
@@ -0,0 +1,21 @@
+staff = "<:staff:867907835546959912>"
+
+secure = "<:secure:867907835815526421>"
+
+on = "<:on:870376486148923392>"
+
+off = "<:off:870376486257954826>"
+
+ondot = "<:1_:870376510232616970>"
+
+offdot = "<:2_:870376510106779708>"
+
+loading = "<a:loading:867907835260960799>"
+
+discordCoder = "<:discordCoder:867907834917683200>"
+
+discordPartner = "<:discordPartner:867907835173666836>"
+
+github_emoji = "<:github:868169388560359434>"
+
+error_emoji = "<:error:870396603062714388>"
