cfb3a0a8f2d290a6181b693d0d2d810f699e233e
Yuutokata <77214089+Yuutokata@users.noreply.github.com>
2021-07-30 00:38:55 +0200

    Update commands.py

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,33 +1,20 @@
-from discord.ext import commands
-import discord
 import datetime
+import os
+
+import discord
+import psutil
+from discord.ext import commands
+from discord_components import (
+    Button,
+    ButtonStyle)
+
+from utils.emoji import *
 
 
-class Cogs(commands.Cog):
+class Commands(commands.Cog, name="Commands"):
 
     def __init__(self, client):
         self.client = client
-
-
-
-    @commands.command()
-    async def info(self, message):
-        bot = self.client.get_user(867108673146847272)
-        me = self.client.get_user(437669166216904715)
-        embed = discord.Embed(
-            title="Bot Info", description=f"If you want to get more info about the Owner/Developer do **{self.client.command_prefix}dev** or **/dev**")
-        embed.add_field(name="Prefix", value=f"`{self.client.command_prefix} or /`\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
-        embed.add_field(name="Latency", value=f"`{round(self.client.latency * 1000)}ms`\n⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
-        embed.add_field(name="Developer", value=f"**[{me.name}](https://discord.com/users/437669166216904715)**\n⠀⠀⠀⠀⠀⠀⠀", inline=True)
-        embed.add_field(name="Code Language", value="**Python**\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
-        embed.add_field(name="Api", value="**Discord.py**\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
-        embed.add_field(name="Bot Version", value="**1.0.0 Beta**\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
-        embed.add_field(name="Creation Date", value="**__<t:1626739200:d>__**", inline=True)
-        embed.add_field(name="Release Date", value="`None`", inline=True)
-        embed.timestamp = datetime.datetime.now()
-        embed.set_footer(text="Chifuyu Matsuno", icon_url=bot.avatar_url)
-        embed.set_thumbnail(url=bot.avatar_url)
-        await message.send(embed=embed)
 
     @commands.command()
     async def dev(self, message):
@@ -36,7 +23,7 @@ class Cogs(commands.Cog):
         yokaisbot = self.client.get_user(856505439034146847)
         me = self.client.get_user(437669166216904715)
         embed = discord.Embed(
-            title="Developer Info")
+            title=f"Developer Info {discordCoder}")
         embed.add_field(name="Name", value=f"**{me.name}**\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
         embed.add_field(name="Account Creation Date", value=f"__<t:1524355200:d>__\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
         embed.add_field(name="Links", value="__[My Links](https://links.yuuto.de/)__\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
@@ -49,23 +36,30 @@ class Cogs(commands.Cog):
         await message.send(embed=embed)
 
     @commands.command()
-    async def Code(self, message):
+    async def code(self, message):
         embed = discord.Embed(
-            title="Source Code", color=discord.Color.light_grey())
+            title=f"Source Code {github_emoji}", color=discord.Color.light_grey())
         embed.add_field(name="Github", value="[Source Code](https://github.com/Yuutokata/Chifuy-Matsuno)", inline=False)
-        embed.add_field(name="License", value="You can use the Code in your Code but just please don't say that you codet it", inline=False)
+        embed.add_field(name="License",
+                        value="You can use the Code in your Code but just please don't say that you codet it",
+                        inline=False)
         embed.timestamp = datetime.datetime.now()
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/867501733084135474/867502083086483497/GitHub-Mark-Light-120px-plus.png")
-        embed.set_footer(icon_url="https://cdn.discordapp.com/attachments/867501733084135474/867502083086483497/GitHub-Mark-Light-120px-plus.png", text="Github Code")
+        embed.set_thumbnail(
+            url="https://cdn.discordapp.com/attachments/867501733084135474/867502083086483497/GitHub-Mark-Light-120px-plus.png")
+        embed.set_footer(
+            icon_url="https://cdn.discordapp.com/attachments/867501733084135474/867502083086483497/GitHub-Mark-Light-120px-plus.png",
+            text="Shouganari ©")
         await message.send(embed=embed)
 
     @commands.command()
-    async def userinfo(self, message, args1 : discord.Member):
+    async def userinfo(self, message, args1: discord.Member):
         embed = discord.Embed(
             title=args1,
             description=args1.mention)
-        embed.add_field(name="⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀__ID__", value=f"```⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀{args1.id}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀```\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=False)
-        embed.add_field(name="Registered", value=f"<t:{round(args1.created_at.timestamp())}:d>\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
+        embed.add_field(name="⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀__ID__", value=f"```⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀{args1.id}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀```\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+                        inline=False)
+        embed.add_field(name="Registered", value=f"<t:{round(args1.created_at.timestamp())}:d>\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+                        inline=True)
         embed.add_field(name="Joined", value=f"<t:{round(args1.joined_at.timestamp())}:d>\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
         embed.add_field(name="Highest Role", value=f"{args1.top_role.mention}\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
         embed.set_thumbnail(url=args1.avatar_url)
@@ -74,13 +68,75 @@ class Cogs(commands.Cog):
         await message.send(embed=embed)
 
     @commands.command()
-    async def userid(self, message, args1 : discord.Member):
+    async def userid(self, message, args1: discord.Member):
         embed = discord.Embed(
             title=args1.name, description=args1.mention)
-        embed.add_field(name="⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀__ID__", value=f"```⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀{args1.id}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀```\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=False)
+        embed.add_field(name="⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀__ID__", value=f"```⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀{args1.id}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀```\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+                        inline=False)
         embed.set_thumbnail(url=args1.avatar_url)
         embed.timestamp = datetime.datetime.now()
+        embed.set_footer(text="Shouganari ©")
         await message.send(embed=embed)
 
+    @commands.command()
+    async def guilds(self, message):
+        embed = discord.Embed(title="Guilds")
+        embed.add_field(name="Guild Count", value=f"{len(self.client.guilds)} Guilds")
+        button = [
+            Button(style=ButtonStyle.URL, url="https://github.com/Shouganari/Chifuyu-Matsuno/", label="Source Code")]
+        await message.send(embed=embed, components=button)
+
+    @commands.command()
+    async def users(self, message):
+        embed = discord.Embed(title="Users")
+        embed.add_field(name="User Count", value=f"{len(self.client.users)} Users")
+        button = [
+            Button(style=ButtonStyle.URL, url="https://github.com/Shouganari/Chifuyu-Matsuno/", label="Source Code")]
+        await message.send(embed=embed, components=button)
+
+    @commands.command()
+    async def about(self, ctx):
+        button = [Button(style=ButtonStyle.URL,
+                         url="https://discord.com/api/oauth2/authorize?client_id=867108673146847272&permissions=8&scope=bot",
+                         label="Invite")]
+        embed = discord.Embed(title=f"Info about Chifuyu Matsuno")
+        embed.add_field(
+            name="Latest Updates",
+            value="Vote Now at [top.gg](https://top.gg/bot/756257170521063444/vote)",
+            inline=False)
+        pid = psutil.Process(os.getpid())
+        used = pid.memory_info().rss / 1024 ** 2
+        total = psutil.virtual_memory().total / 1024 ** 2
+        bot = self.client.get_user(867108673146847272)
+        developer = self.client.get_user(437669166216904715)
+        embed.add_field(name="Developer", value=f"[{developer.name}](https://discord.com/users/437669166216904715)")
+        embed.add_field(name="Code Language", value="**Python**")
+        embed.add_field(name="Bot Version", value="**1.0.0 Beta**")
+        embed.add_field(name="Creation Date", value="**__<t:1626739200:d>__**")
+        embed.add_field(name="Release Date", value="`None`")
+        embed.add_field(name="Ping", value=f"`{round(self.client.latency * 1000)}ms`")
+        embed.add_field(name="Guild Count", value=f"{len(self.client.guilds)} Guilds")
+        embed.add_field(name="User Count", value=f"{len(self.client.users)} Users")
+        embed.add_field(name="CPU Usage", value=f"{psutil.cpu_percent()}%")
+        embed.add_field(name="Memory Usage", value=f"{used:,.2f}/{round(total)} MB")
+        embed.add_field(
+            name="Bot Invite",
+            value=f"Not Released",
+        )
+        embed.add_field(name="Commands", value=f"{len(self.client.commands)} loaded")
+        credits_list = [
+            (self.client.get_user(437669166216904715), 'Owner/Developer'),
+            (self.client.get_user(774286355580583957), 'Tester'),
+        ]
+        embed.add_field(
+            name="Credits",
+            value="\n\r".join(f"{user} ({role})" for user, role in credits_list)
+        )
+        embed.timestamp = datetime.datetime.now()
+        embed.set_footer(text="Shouganari ©", icon_url=bot.avatar_url)
+        embed.set_thumbnail(url=bot.avatar_url)
+        await ctx.send(embed=embed)
+
+
 def setup(client):
-    client.add_cog(Cogs(client))
+    client.add_cog(Commands(client))
